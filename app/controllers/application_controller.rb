423384ba6c0c7754aa247d7dcb2bd64e7800e2b3
hellomundo <ed.ortega@gmail.com>
2015-11-06 20:37:30 -0800

    added stats to homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  # authenticate
   before_action :authenticate_user!
 
   def index
-    # how many donors have been followed up with
+    total_donors = Donor.count
     # how many donors have donated
+    @committed_donors = Donor.where(status: 4).count
+    # how many donors have been followed up with
+    @uncommitted_donors = total_donors - @committed_donors
+    # how may items have been donated
+    @items_donated = Item.count
     # what is the overall value of donations
-    # 
+    @total_value = Item.sum("fmv")
+
   end
 
 end
