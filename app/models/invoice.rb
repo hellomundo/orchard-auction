03d69a0fa500091bfca975b08a0320886c877340
hellomundo <ed.ortega@gmail.com>
2017-02-17 18:49:52 -0800

    trying to fix bug crashing heroku

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,24 +4,6 @@ class Invoice < ActiveRecord::Base
   has_many :pledges
   has_many :wins
 
-  before_create :initialize_order_status
-  before_save :calculate_subtotal
-
-  enum status: {
-      unpaid: 0,
-      paid: 1
-    }
-
   private
-
-  initialize_order_status
-    self.status = :unpaid
-  end
-
-  calculate_subtotal
-    # add up wins
-    # add up pledges
-    self.subtotal = 0
-  end
 
 end
