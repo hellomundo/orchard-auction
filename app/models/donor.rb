74a5accf813fb4fe8e1ba0db85903fe0ece54b42
hellomundo <ed.ortega@gmail.com>
2017-02-21 15:35:30 -0800

    fixing donor sort problem

--- app/models/donor.rb
+++ app/models/donor.rb
@@ -7,7 +7,7 @@ class Donor < ActiveRecord::Base
 
   def self.by_event(event_id)
     #joins(:stage).where('statuses.event_id = ?', event_id)
-    joins("LEFT OUTER JOIN statuses ON donors.id = statuses.donor_id").includes(:stage)
+    joins("LEFT OUTER JOIN statuses ON donors.id = statuses.donor_id").where('statuses.donor_id IS NULL OR statuses.event_id = ?', event_id).includes(:stage)
   end
 
   def self.by_event_and_name(event_id, name)
